71e4e99fffc5246e66d7a948e798a4dd1f16667b
Karin Touchie <ktouchie@gmail.com>
2015-12-11 15:57:16 +0100

    Change class name, fix var note bug

--- Exercice2.java
+++ Exercice2.java
@@ -1,13 +1,14 @@
 import java.util.Scanner;
-class Exercice1 {
+class Exercice2 {
     
     public static void main(String [] args) {
         Scanner sc = new Scanner(System.in);
         int highest = 0;
+        int note = 0;
         
         while (note>=0) {            
             System.out.println("Entrez une note sur 20:");
-            int note = sc.nextInt();
+            note = sc.nextInt();
             if (note>highest) {
                 highest = note;
             }
