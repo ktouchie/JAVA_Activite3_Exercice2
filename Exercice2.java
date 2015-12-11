f394fe3fd6eabecc54abf5f790cf895a42bb4cac
Karin Touchie <ktouchie@gmail.com>
2015-12-11 15:52:22 +0100

    Fix loop to include prompt, add result output

--- Exercice2.java
+++ Exercice2.java
@@ -3,17 +3,17 @@ class Exercice1 {
     
     public static void main(String [] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Entrez une note sur 20:");
-        int note = sc.nextInt();
         int highest = 0;
         
-        while (note>=0) {
+        while (note>=0) {            
+            System.out.println("Entrez une note sur 20:");
+            int note = sc.nextInt();
             if (note>highest) {
                 highest = note;
             }
         }
         
-        System.out.println("")
-        
+        System.out.println("La note la plus grande est " + highest);
+               
     }
 }
